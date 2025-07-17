f729f68999f19e7a5ca268fdcc091893f7372717
SHAMMAS <muhdshammas612@gmail.com>
2025-07-17 10:26:45 +0530

    solved AttributeError

--- invoiceApp/models.py
+++ invoiceApp/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-import datetime
 # Create your models here.
 
 class Invoice(models.Model):
     customer_name = models.CharField(max_length=150)
-    date = models.DateTimeField(default=datetime.date.today)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.customer_name
